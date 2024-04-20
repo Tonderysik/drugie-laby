4e1f9ad569191f2de5488cd25bf4f9d5ce5340dd
studentwsb <gax36304@student.gdynia.merito.pl>
2024-04-20 12:40:02 +0200

    first commit

---
+++ src/Bayer.java
@@ -0,0 +1,9 @@
+public class Bayer extends BaseAbstract {
+    protected String bName;
+    protected int bYear;
+
+    Bayer(String bName, int bYear) {
+        this.bName = bName;
+        this.bYear = bYear;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello World!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Car car = new Car();
+        System.out.println(car.InfoCar());
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Car car2 = new Car("Różowy");
+        System.out.println(car2.InfoCar());
+
+        Car car3 = new Car("Czarny", 2024);
+        System.out.println(car3.InfoCar());
+
     }
 }
